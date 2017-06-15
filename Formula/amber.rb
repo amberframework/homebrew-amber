0641511e4bbc5926f30d7dcb8a9caeeb891aff04
Elias Perez <eliasjpr@users.noreply.github.com>
2017-06-15 15:39:30 -0400

    Update SHA

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -2,7 +2,7 @@ class Amber < Formula
   desc "Amber CLI client for generating, scaffolding Amber web apps."
   homepage "https://www.ambercr.io"
   url "https://github.com/Amber-Crystal/amber_cmd/archive/v0.1.15.tar.gz"
-  sha256 "2b56718bf1623dc03d25183544d9ecf2f980ac60407e6c7320d83bcb4102b99c"
+  sha256 "53cd5a3961658999523ab4045e30512585c61a3a2db0d0015f5414de7d2ffac2"
 
   depends_on "crystal-lang"
   depends_on "openssl"
