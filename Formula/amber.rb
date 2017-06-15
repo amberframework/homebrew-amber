5d1b1440078f468b68859722b90687caf5bc6ea8
Elias Perez <eliasjpr@users.noreply.github.com>
2017-06-15 15:33:30 -0400

    Adds Release latest URL
    https://github.com/Amber-Crystal/amber_cmd/releases/latest

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -1,7 +1,7 @@
 class Amber < Formula
   desc "Amber CLI client for generating, scaffolding Amber web apps."
   homepage "https://www.ambercr.io"
-  url "https://github.com/Amber-Crystal/amber_cmd/archive/v0.1.13.tar.gz"
+  url "https://github.com/Amber-Crystal/amber_cmd/releases/latest"
   sha256 "2b56718bf1623dc03d25183544d9ecf2f980ac60407e6c7320d83bcb4102b99c"
 
   depends_on "crystal-lang"
