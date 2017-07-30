b341c677f9943d20987ad30b693a3828eb873e3b
Isaac Sloan <isaac@isaacsloan.com>
2017-07-30 00:11:24 -0600

    updated homebrew to new release

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -1,9 +1,9 @@
 class Amber < Formula
   desc "Amber CLI client for generating, scaffolding Amber web apps."
-  version "0.1.17"
+  version "0.1.19"
   homepage "https://www.ambercr.io"
-  url "https://github.com/Amber-Crystal/amber_cmd/archive/v#{version}.tar.gz"
-  sha256 "721d8d9dbd13df2187e7d6ffad5b00894908dd18dbc90d65c85272422f84f4a5"
+  url "https://github.com/Amber-Crystal/amber-cmd/archive/v#{version}.tar.gz"
+  sha256 "c5a921757c99207006a2213505557780005a3b15832b98e01d99f34fcdaadb06"
 
   depends_on "crystal-lang"
   depends_on "openssl"
