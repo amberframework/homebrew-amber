91fb8248f4af068f922a6a3417e2ff5336863a18
Isaac Sloan <isaac@isaacsloan.com>
2017-08-03 12:42:20 -0600

    fixed homebrew

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -5,7 +5,7 @@ class Amber < Formula
   version "0.1.20"
   homepage "https://www.ambercr.io"
   url "https://github.com/Amber-Crystal/amber-cmd/archive/v#{version}.tar.gz"
-  sha256 "45ded959182ef17ac7a106bd653f6570a2d194f6d0900a1b16273c302caca33e"
+  sha256 "d39bf693dfdac6c4ca6d0ebb15dfb1323b86c695552d013d48f502d46aeb2201"
 
   depends_on "crystal-lang"
   depends_on "openssl"
