f51e942317146060277e2c79ef8d861d56c57aea
Isaac Sloan <isaac@isaacsloan.com>
2017-08-13 10:19:13 -0600

    updated version number and sha

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -2,10 +2,10 @@
 # brew fetch Formula/amber.rb --build-from-source
 class Amber < Formula
   desc "Amber CLI client for generating, scaffolding Amber web apps."
-  version "0.1.20"
+  version "0.1.21"
   homepage "https://www.ambercr.io"
   url "https://github.com/Amber-Crystal/amber-cmd/archive/v#{version}.tar.gz"
-  sha256 "d39bf693dfdac6c4ca6d0ebb15dfb1323b86c695552d013d48f502d46aeb2201"
+  sha256 "8fc7bdd55421c74b715e188014eb1bd37dc9975365d9f4c115087a600213b8b6"
 
   depends_on "crystal-lang"
   depends_on "openssl"
