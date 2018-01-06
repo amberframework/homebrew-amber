8958c2042fbc3ca1b73b9b7118ec270a7074907d
Isaac Sloan <isaac@isaacsloan.com>
2018-01-05 18:23:44 -0700

    updated formula

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -6,7 +6,7 @@ class Amber < Formula
   version ENV['AMBER_VERSION'] || JSON.parse(open("https://api.github.com/repos/amberframework/amber/releases/latest").read)["tag_name"]
   homepage "https://amberframework.org"
   url "https://github.com/amberframework/amber/archive/#{version}.tar.gz"
-  sha256 "edeed6d43524a79948ac3e307340d6cea56dd9c4009e4c946c4cf395c9a6ac1d"
+  sha256 "6a27b22705addc15e60394b6228e8b9040c29acb514ca6a949cc45355f19dc10"
 
   depends_on "crystal-lang"
 
@@ -17,6 +17,6 @@ class Amber < Formula
   end
 
   test do
-    assert_match "Amber CLI (amberframework.org) - v0.6.0", shell_output("#{bin}/amber -v")
+    assert_match "Amber CLI (amberframework.org) - v0.6.1", shell_output("#{bin}/amber -v")
   end
 end
