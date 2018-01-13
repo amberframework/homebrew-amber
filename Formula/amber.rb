ff46593a3c36a47f17dc4f0421233dd496b6526e
Isaac Sloan <isaac@isaacsloan.com>
2018-01-12 22:52:07 -0700

    updated brew formula

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -6,8 +6,7 @@ class Amber < Formula
   version ENV['AMBER_VERSION'] || JSON.parse(open("https://api.github.com/repos/amberframework/amber/releases/latest").read)["tag_name"]
   homepage "https://amberframework.org"
   url "https://github.com/amberframework/amber/archive/#{version}.tar.gz"
-  sha256 "6a27b22705addc15e60394b6228e8b9040c29acb514ca6a949cc45355f19dc10"
-
+  sha256 "188790b260accb35e4f077abd1c37eafef9aedd9b580194a56e6d95edb4f5e51"
   depends_on "crystal-lang"
 
   def install
