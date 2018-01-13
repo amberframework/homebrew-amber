4248622d2516ab8de4a09d44a61a5cf021f495e7
Elias Perez <eliasjpr@users.noreply.github.com>
2018-01-13 11:18:16 -0500

    Removes dynamic bits
    Some users are experiencing and GitHub API Rate Limit Reached error
    This removes hitting the GitHub API to request the version and updates the formula
    to the Homebrew Core formula submitted (https://github.com/Homebrew/homebrew-core/pull/21895/files)

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -1,21 +1,33 @@
-require 'open-uri'
-require 'json'
-
 class Amber < Formula
-  desc "Amber CLI client for generating, scaffolding Amber web apps."
-  version ENV['AMBER_VERSION'] || JSON.parse(open("https://api.github.com/repos/amberframework/amber/releases/latest").read)["tag_name"]
-  homepage "https://amberframework.org"
-  url "https://github.com/amberframework/amber/archive/#{version}.tar.gz"
+  desc "CLI client for generating, scaffolding Amber web applications"
+  homepage "https://www.amberframework.org"
+  url "https://github.com/amberframework/amber/archive/v0.6.3.tar.gz"
   sha256 "f65711e2fdc473bd77142acce8c383e591d87ab666f81f1008973b4b2aebf353"
+
   depends_on "crystal-lang"
 
   def install
     system "shards", "install"
     system "make", "install", "PREFIX=#{prefix}"
-    bin.install "bin/amber"
   end
 
   test do
-    assert_match "Amber CLI (amberframework.org) - #{version}", shell_output("#{bin}/amber -v")
+    output = shell_output("#{bin}/amber new test_app")
+    %w[
+      config/environments
+      amber.yml
+      shard.yml
+      public
+      src/controllers
+      src/views
+      src/assets
+      src/test_app.cr
+    ].each do |path|
+      assert_match path, output
+    end
+    cd "test_app" do
+      build_app = shell_output("shards build test_app")
+      assert build_app, "Building: test_app"
+    end
   end
 end
