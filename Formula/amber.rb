7a36a6165fe361f76c0c0368775849b54fb4a7d1
Isaac Sloan <isaac@isaacsloan.com>
2018-10-18 17:24:28 -0700

    updated formula for 0.10.0

--- Formula/amber.rb
+++ Formula/amber.rb
@@ -1,8 +1,8 @@
 class Amber < Formula
   desc "CLI client for generating and scaffolding Amber web applications"
   homepage "https://www.amberframework.org/"
-  url "https://github.com/amberframework/amber/archive/v0.9.0.tar.gz"
-  sha256 "b99184e0bc479f6c79a9f2fa6364d1dfbdd06d53c3683d1c214138dfe1b6c409"
+  url "https://github.com/amberframework/amber/archive/v0.10.0.tar.gz"
+  sha256 "4b35f94690832d123a496b7620984374559d59cb9bc8cdc3462e78da76eb5097"
 
   depends_on "crystal"
 
